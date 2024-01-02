36ca9daa51c4c623857293213c7e433c310c3898
webwatchtech <155053338+webwatchtech@users.noreply.github.com>
2024-01-03 00:48:21 +0530

    Add files via upload

---
+++ js/script1.js
@@ -0,0 +1,17 @@
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 1,
+  grabCursor: true,
+  loop: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  autoplay: {
+    delay: 3000, // Delay between transitions (in milliseconds)
+    disableOnInteraction: false, // Whether to disable autoplay on user interaction
+  },
+});
